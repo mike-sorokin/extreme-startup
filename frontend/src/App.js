680492cd32d3f66e5424845fb1a5e06332df49de
dks20 <dks20@ic.ac.uk>
2022-10-14 14:18:23 +0100

    Create player add from

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
+// import { useState } from "react";
 import './App.css';
 
 function App() {
+
+  // const [name, setName] = useState("")
+  // const [url, setUrl] = useState("")
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Add a new player</h1>
+        <form method="post" action="https://extreme-restartup.fly.dev/players">
+            <label htmlFor="name">Name: </label>
+            <input type="text" id="name" name="name"/>
+
+            <label htmlFor="url">URL: </label>
+            <input type="text" id="url" name="url" placeholder="http://...."/>
+
+            <input type="submit" value="Submit" />
+        </form>
     </div>
   );
 }
