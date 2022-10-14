db4ec58f60ec1b9ba7098a043024a54c7713d929
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-10-14 11:43:15 +0100

    Add player personal page and player withdraw functionality

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,5 @@
 from crypt import methods
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flaskr.player import Player
 from flaskr.scoreboard import Scoreboard
 import os
@@ -12,27 +12,47 @@ app = Flask(__name__)
 players = {}
 # scoreboard = Scoreboard(os.getenv('LENIENT'))
 scoreboard = {}
+player_threads = {}
+
 
 @app.route("/")
 def index():
-    return render_template('leaderboard.html', leaderboard=scoreboard)
+    return render_template("leaderboard.html", leaderboard=scoreboard)
 
-@app.route("/players", methods=['GET', 'POST'])
+
+@app.route("/players", methods=["GET", "POST"])
 def add_player():
-    if request.method == 'GET':
-        return render_template('add_player.html')
+    if request.method == "GET":
+        return render_template("add_player.html")
     else:
-        player = Player(request.form['name'], request.form['url'])
+        player = Player(request.form["name"], request.form["url"])
         # scoreboard.new_player(player)
         scoreboard[player] = 0
         players[player.uuid] = player
         player_thread = threading.Thread(target=sendQuestion, args=(player,))
+        player_threads[player.uuid] = player_thread
         player_thread.start()
-        return render_template('player_added.html')
+        return render_template("player_added.html", player_id=player.uuid)
+
+
+@app.get("/players/<id>")
+def player_page(id):
+    player = players[id]
+    return render_template("personal_page.html", name=player.name, id=player.uuid)
+
+
+@app.get("/withdraw/<id>")
+def remove_player(id):
+    assert id in player_threads
+    del player_threads[id]
+    del scoreboard[players[id]]
+    del players[id]
+    return redirect("/")
+
 
 def sendQuestion(player):
     while True:
-        r = requests.get(player.url, params={'q': "What is your name?"}).text
+        r = requests.get(player.url, params={"q": "What is your name?"}).text
         if r == player.name:
             scoreboard[player] += 2
         else:
