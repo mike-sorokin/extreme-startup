0ee55baecfb97fbe126598150449c657af5ecb84
Jamie Todd <jmt20@ic.ac.uk>
2022-10-17 13:02:21 +0100

    Mantine + Notifications added, player form updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
+import axios from 'axios';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { MantineProvider } from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
 import Home from "./components/Home"
 import Player from "./components/Player"
 
@@ -6,17 +9,17 @@ import './App.css';
 
 function App() {
 
-  // const [name, setName] = useState("")
-  // const [url, setUrl] = useState("")
-
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/player/:id" element={<Player />} />
-      </Routes>
-    </Router>
-
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <NotificationsProvider position='top-right'>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/player/:id" element={<Player />} />
+          </Routes>
+        </Router>
+      </NotificationsProvider>
+    </MantineProvider>
   );
 }
 
