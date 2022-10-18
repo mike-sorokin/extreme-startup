5647480e59f7a3f07fdcbf6bdac7f2ce168f7cd1
dks20 <dks20@ic.ac.uk>
2022-10-18 13:10:12 +0100

    add basic player page

--- frontend/src/components/Player.js
+++ frontend/src/components/Player.js
@@ -5,9 +5,21 @@ function Player() {
 
   const params = useParams()
 
+  const deletePlayer = (id) => {
+    console.log("deleted player", id)
+  }
+
+  const events = {
+    event1: "test1",
+    event2: "test2"
+  }
+
   return (
   <div>
-    <div>team id: {params.id}</div>
+    <h1> Extreme Restartup Game id: </h1>
+    <div> Hello {params.id}</div>
+    <div> Your score is: </div>
+    <div onClick={deletePlayer(params.id)}> Withdraw</div>
   </div>
   )
 }
