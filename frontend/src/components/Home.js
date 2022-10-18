5947a03f5e7603faf7a4efad93a99191a367b055
dks20 <dks20@ic.ac.uk>
2022-10-18 11:55:47 +0100

    move Home component code to AddPlayer component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -6,62 +6,8 @@ import { showNotification } from '@mantine/notifications';
 import { IconCheck, IconX } from '@tabler/icons';
 
 function Home() {
-  const [name, setName] = useState("");
-  const [url, setUrl] = useState("");
-
-  const addPlayer = (e) => {
-    e.preventDefault();
-
-    if (name.trim() === "") {
-      showNotification({  
-        title: "Error creating player",
-        message: "You forgot to enter a name!",
-        icon: <IconX size={18} />, 
-        color: "red"
-      });
-
-      return
-    }
-
-    const playerObject = {
-      name: name.trim(),
-      url: url.trim(),
-    };
-
-    axios
-      .post("https://extreme-restartup.fly.dev/players", playerObject)
-      .then(() => {
-        showNotification({
-          title: "Success",
-          message: "Player successfully created!",
-          icon: <IconCheck size={18} />,
-          color: "teal"
-        });
-      })
-      .catch((err) => alert(err.response.data));
-    
-      // TODO: Get player id from backend and redirect to player page
-  }
-
-  return (
-    <div className="Home">
-      <h1>Add a new player</h1>
-        {/* <form method="post" action="https://extreme-restartup.fly.dev/players">
-            <label htmlFor="name">Name: </label>
-            <input type="text" id="name" name="name"/>
-
-            <label htmlFor="url">URL: </label>
-            <input type="text" id="url" name="url" placeholder="http://...."/>
-
-            <input type="submit" value="Submit" />
-        </form> */}
-        
-        <form onSubmit={addPlayer}>
-          <TextInput value={name} onChange={(e) => setName(e.target.value)} placeholder="Your player name" label="Enter player name:" required/>
-          <TextInput value={url} onChange={(e) => setUrl(e.target.value)} placeholder="Your URL (http://...)" label="Enter URL:" required/>
-          <Button type="submit">Submit</Button>
-        </form>
-    </div>
+  return(
+    <h1>Home</h1>
   )
 }
 
