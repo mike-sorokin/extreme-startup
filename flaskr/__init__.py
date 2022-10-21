10fb299e296f1a11d666a97f266330153e7833d6
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-10-21 08:43:05 +0100

    Edit __init__.py to integrate with frontend

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -99,10 +99,7 @@ def all_players(game_id):
 
     elif request.method == "POST":  # create a new player -- initialise thread
         # player = Player(game_id, request.form["name"], api=request.form["api"])
-        print(request.is_json, request.json, request.get_json())
-        player = Player(
-            game_id, request.get_json()["name"], api=request.get_json()["api"]
-        )
+        player = Player(game_id, request.form["name"], api=request.form["api"])
         games[game_id].new_player(player.uuid)
         scoreboards[game_id].new_player(player)
         players[player.uuid] = player
