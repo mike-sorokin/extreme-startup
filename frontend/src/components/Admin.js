38caaefbe4cdce6fe60307eddff35919ff93ae2c
dksethia <41707087+dksethia@users.noreply.github.com>
2022-10-21 14:43:02 +0100

    admin buttons

--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Button } from "@mantine/core"
 
 function Admin() {
+
+  const pauseGame = () => {
+    console.log("pause")
+  }
+
+  const advanceRound = () => {
+    console.log("advance")
+  }
+
   return (
-    <div>Admin</div>
+    <div>
+      <h1>Admin</h1>
+      <Button onClick={pauseGame()}>Pause Game</Button>
+      <Button onClicc={advanceRound()}>Advance Round</Button>
+    </div>
   )
 }
 
