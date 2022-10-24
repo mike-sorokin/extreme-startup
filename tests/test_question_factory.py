f476b259e23203a764f4b216dc6c8b63dee14643
Andrey Popov <ap4220@ic.ac.uk>
2022-10-24 15:22:52 +0100

    [fix/ap4220] - fixed test import

--- tests/test_question_factory.py
+++ tests/test_question_factory.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append(".")
 
-from flaskr.question_factory import QuestionFactory
+from flaskr.question_factory import QuestionFactory, MAX_ROUND
 from flaskr.questions import WarmupQuestion, Question
 import pytest
 import random
@@ -31,14 +31,14 @@ def test_zero_round_asks_warmup_question_only():
 
 
 def test_1st_round_asks_1st_2nd_question_only():
-    first_round = QuestionFactory(1) 
+    first_round = QuestionFactory(1)
     assert first_round.window_end == 2
-    assert first_round.window_start == 0 
+    assert first_round.window_start == 0
 
 def test_2st_round_asks_1st_to_4th_question_only():
     second_round = QuestionFactory(2)
     assert second_round.window_end == 4
-    assert second_round.window_start == 0 
+    assert second_round.window_start == 0
 
 def test_n_plus_first_round_shifts_nth_round_window_by_two():
     n = random.randint(1, MAX_ROUND - 1)
