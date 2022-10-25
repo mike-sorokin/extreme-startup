900ff2c0b30d82574430ab40045ae5d0e837de05
dksethia <dksethia1@gmail.com>
2022-10-26 00:41:32 +0100

    refactor GoToGame

--- frontend/src/components/GoToGame.js
+++ frontend/src/components/GoToGame.js
@@ -1,22 +1,36 @@
-import { gamePageUrl } from '../utils/urls'
 import React from 'react';
-import { Button } from '@mantine/core';
 import { useNavigate } from 'react-router-dom'
+import { Button } from '@mantine/core';
 
-function GoToGame(gameIdGetter) {
+import { game } from '../utils/urls'
+
+// Do we need this component? Why don't we just redirect as soon as a game is made?
+function GoToGame({ gameId }) {
 
   const navigate = useNavigate();
 
-  const goToGamePage = () => {
-    navigate(gamePageUrl(gameIdGetter.getGameId()))
-  }
-
   return (
     <div>
-      <p>Your game id is: {gameIdGetter.getGameId()}</p>
-      <Button type="button" onClick={goToGamePage}>To Game Page</Button>
+      <p>Your game id is: {gameId}</p>
+      <Button type="button" onClick={() => { navigate(game(gameId)) }}>To Game Page</Button>
     </div>
   )
 }
+
+// function GoToGame(gameIdGetter) {
+
+//   const navigate = useNavigate();
+
+//   const goToGamePage = () => {
+//     navigate(gamePageUrl(gameIdGetter.getGameId()))
+//   }
+
+//   return (
+//     <div>
+//       <p>Your game id is: {gameIdGetter.getGameId()}</p>
+//       <Button type="button" onClick={goToGamePage}>To Game Page</Button>
+//     </div>
+//   )
+// }
 
 export default GoToGame
