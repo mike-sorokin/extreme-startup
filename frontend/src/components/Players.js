6a8af0402bc9d0ca5c1334dea22a9f4481d10848
dksethia <dksethia1@gmail.com>
2022-10-26 00:42:30 +0100

    refactor Players.js

--- frontend/src/components/Players.js
+++ frontend/src/components/Players.js
@@ -1,92 +1,127 @@
-import React, {useEffect, useState} from 'react'
-import { useParams } from "react-router-dom"
+import React, { useEffect, useState } from 'react'
+import { useParams, useNavigate } from "react-router-dom"
 import { Button, Container, Table } from '@mantine/core'
 import axios from 'axios'
-import { useNavigate } from "react-router-dom";
-import { playerPageUrl, playerCreationUrl } from '../utils/urls'
+
+import { deleteAllPlayers, deletePlayer, fetchAllPlayers } from '../utils/requests'
 
 function Players() {
-    const params = useParams();
-    const [players, setPlayers] = useState([])
-    const [refreshTimer, setRefreshTimer] = useState(0)
-    const navigate = useNavigate();
+  const [players, setPlayers] = useState([])
+  const [refreshTimer, setRefreshTimer] = useState(0)
 
-    useEffect(() => {
-        getPlayers()
-        setTimeout(() => setRefreshTimer(prevState => prevState + 1), 1000)
-      }, [refreshTimer]);
+  const params = useParams();
+  const navigate = useNavigate();
 
-    function getPlayers() {
-        axios.get(playerCreationUrl(params.gameid))
-        .then(function (response) {
-          console.log(response);
-          const temp = []
-          for (let [id, player] of Object.entries(response.data.players)) {
-              temp.push({'id': id, 'name': player.name, 'api':player.api})
-          }
-          console.log(temp)
-          setPlayers(temp)
-          console.log(players)
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+  useEffect(() => {
+    const getPlayers = async () => {
+      try {
+        const response = await fetchAllPlayers(params.gameid)
+        setPlayers(response)
+      } catch (error) {
+        // TODO
+      }
     }
 
-    function withdrawPlayer(playerid) {
-        axios.delete("/api" + playerPageUrl(params.gameid, playerid))
-        .then(function (response) {
-          console.log(response);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+    getPlayers()
+
+    setTimeout(() => setRefreshTimer(prevState => prevState + 1), 1000)
+  }, [refreshTimer])
+
+
+  // useEffect(() => {
+  //   getPlayers()
+  //   setTimeout(() => setRefreshTimer(prevState => prevState + 1), 1000)
+  // }, [refreshTimer]);
+
+  // function getPlayers() {
+  //   axios.get(playerCreationUrl(params.gameid))
+  //     .then(function (response) {
+  //       console.log(response);
+  //       const temp = []
+  //       for (let [id, player] of Object.entries(response.data.players)) {
+  //         temp.push({ 'id': id, 'name': player.name, 'api': player.api })
+  //       }
+  //       console.log(temp)
+  //       setPlayers(temp)
+  //       console.log(players)
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // }
+
+  const withdrawPlayer = async (playerId) => {
+    try {
+      const response = await deletePlayer(params.gameid, playerId)
+      console.log(response)
+    } catch (error) {
+      // TODO
     }
-
-    function withdrawPlayers() {
-        axios.delete(playerCreationUrl(params.gameid))
-        .then(function (response) {
-          console.log(response);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    }
-
-    return (
-      <Container size="xl" px="sm">
-        <h3>Players</h3>
-        <Button variant="outline" color="red" radius="md" size="md" 
-                onClick={() => withdrawPlayers()}>Withdraw All</Button>
-        <hr/>
-
-        <Table hover>
-          <thead>
-              <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>API</th>
-              <th>ACTION</th>
-              </tr>
-          </thead>
-          <tbody>
-            { players.map(({name, api, id}) => (
-              <tr key={id} onClick={() => navigate(id)}>
-                <td>{id}</td>
-                <td>{name}</td>
-                <td>{api}</td>
-                <td>
-                  <Button variant="outline" color="red" radius="md" size="md" 
-                    onClick={() => withdrawPlayer("./" + id)}>
-                    Withdraw
-                  </Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </Container>
-    )
   }
 
-  export default Players
+  // function withdrawPlayer(playerid) {
+  //   axios.delete("/api" + playerPageUrl(params.gameid, playerid))
+  //     .then(function (response) {
+  //       console.log(response);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // }
+
+  const withdrawAllPlayers = async () => {
+    try {
+      const response = await deleteAllPlayers(params.gameid)
+      console.log(response)
+    } catch (error) {
+      // TODO
+    }
+  }
+
+  // function withdrawPlayers() {
+  //   axios.delete(playerCreationUrl(params.gameid))
+  //     .then(function (response) {
+  //       console.log(response);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // }
+
+  return (
+    <Container size="xl" px="sm">
+      <h3>Players</h3>
+      <Button variant="outline" color="red" radius="md" size="md"
+        onClick={() => withdrawAllPlayers()}>Withdraw All</Button>
+      <hr />
+
+      <Table hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>API</th>
+            <th>ACTION</th>
+          </tr>
+        </thead>
+        <tbody>
+          {players.map((player) => (
+            <tr key={player.id} onClick={() => navigate(player.id)}>
+              <td>{player.id}</td>
+              <td>{player.name}</td>
+              <td>{player.api}</td>
+              <td>
+                <Button variant="outline" color="red" radius="md" size="md"
+                  onClick={() => withdrawPlayer(player.id)}>
+                  Withdraw
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  )
+}
+
+export default Players
