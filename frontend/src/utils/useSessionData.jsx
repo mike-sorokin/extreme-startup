a309c91b67737053a47a064fb062edb02ead0979
Andrey Popov <ap4220@ic.ac.uk>
2022-11-11 11:25:42 +0000

    [feat/ap4220] - highlighted player in leaderboard as well. Made useSessionData hook exported

--- frontend/src/utils/useSessionData.jsx
+++ frontend/src/utils/useSessionData.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 
 import { checkAuth } from './requests'
 
-function useSessionData (gameId) {
+export function useSessionData (gameId) {
   const [isAdmin, setIsAdmin] = useState(false)
   const [playerID, setPlayerID] = useState('')
 
