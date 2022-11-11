bf9a7a15e27d838d84878f6118cf9af6d54fd176
Jamie Todd <jmt20@ic.ac.uk>
2022-11-11 14:51:33 +0000

    Confirmation Modal util

---
+++ frontend/src/components/ConfirmationModal.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Button, Center, Modal, Space, Text } from '@mantine/core'
+
+function ConfirmationModal ({ opened, setOpened, title, body, func }) {
+  return <Modal centered
+        opened={opened}
+        onClose={() => setOpened(false)}
+        title={title}
+        closeOnClickOutside={false}>
+        <div>
+          <Text>{body}</Text>
+          <Space h="md" />
+          <Center>
+            <Button variant="filled"
+                color="red"
+                radius="md"
+                size="md"
+                onClick={() => func}>
+                Yes
+            </Button>
+          </Center>
+        </div>
+    </Modal>
+}
+
+export default ConfirmationModal
