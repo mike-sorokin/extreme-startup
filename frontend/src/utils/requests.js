13d1a4808b25c889c19c4b8bbce06005f1d7db2d
Andrey Popov <ap4220@ic.ac.uk>
2022-11-16 17:55:56 +0000

    switching to correct api endpoint /existed

--- frontend/src/utils/requests.js
+++ frontend/src/utils/requests.js
@@ -514,7 +514,7 @@ export async function checkValidPlayer (gameId, playerId) {
  */
 export async function checkGameEnded (gameId) {
   try {
-    const response = await instance.get(gameoverAPI(gameId))
+    const response = await instance.get(reviewAPIs(gameId).existed)
     return response.data.game_over
   } catch (error) {
     alertError(error)
