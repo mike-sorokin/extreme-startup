67593df87ce243543d2dddffafbb2c6d07e882e5
dksethia <41707087+dksethia@users.noreply.github.com>
2022-11-17 16:38:21 +0000

    change colours for top 3 positions

--- frontend/src/components/FinalBoard.jsx
+++ frontend/src/components/FinalBoard.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Card, Container, Space, Table, Title } from '@mantine/core'
 
 function FinalBoard ({ finalBoard }) {
+  const [sortedBoard, setSortedBoard] = useState([])
+
+  useEffect(() => {
+    const sorted = finalBoard?.sort((a, b) => { return b.score - a.score })
+    setSortedBoard(sorted)
+  }, [finalBoard])
+
   return (
     <Card sx={{ maxHeight: '200px', overflow: 'auto' }}>
       <Container size="xl" px="sm">
@@ -21,8 +28,35 @@ function FinalBoard ({ finalBoard }) {
               </tr>
             </thead>
             <tbody>
+              { sortedBoard && sortedBoard.length >= 1 &&
+                <tr style={{ backgroundColor: 'gold' }}>
+                  <td>{sortedBoard[0].player_id}</td>
+                  <td>{sortedBoard[0].name}</td>
+                  <td>{sortedBoard[0].score}</td>
+                  <td>{sortedBoard[0].success_ratio}</td>
+                  <td>{sortedBoard[0].longest_streak}</td>
+                </tr>
+              }
+              { sortedBoard && sortedBoard.length >= 2 &&
+                <tr style={{ backgroundColor: 'silver' }}>
+                  <td>{sortedBoard[1].player_id}</td>
+                  <td>{sortedBoard[1].name}</td>
+                  <td>{sortedBoard[1].score}</td>
+                  <td>{sortedBoard[1].success_ratio}</td>
+                  <td>{sortedBoard[1].longest_streak}</td>
+                </tr>
+              }
+              { sortedBoard && sortedBoard.length >= 3 &&
+                <tr style={{ backgroundColor: 'brown' }}>
+                  <td>{sortedBoard[2].player_id}</td>
+                  <td>{sortedBoard[2].name}</td>
+                  <td>{sortedBoard[2].score}</td>
+                  <td>{sortedBoard[2].success_ratio}</td>
+                  <td>{sortedBoard[2].longest_streak}</td>
+                </tr>
+              }
               {
-                finalBoard?.map((player) => (
+                sortedBoard?.slice(3).map((player) => (
                   <tr key={player.player_id}>
                     <td>{player.player_id}</td>
                     <td>{player.name}</td>
