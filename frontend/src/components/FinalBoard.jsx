9651147d1d2eefc855eb928ebb73cf979b44139a
dksethia <41707087+dksethia@users.noreply.github.com>
2022-11-18 14:44:15 +0000

    add svg icons to template (untested)

--- frontend/src/components/FinalBoard.jsx
+++ frontend/src/components/FinalBoard.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import { Container, Table } from '@mantine/core'
 
+import { ReactComponent as Gold } from '../assets/gold_medal_icon.svg'
+import { ReactComponent as Silver } from '../assets/silver_medal_icon.svg'
+import { ReactComponent as Bronze } from '../assets/bronze_medal_icon.svg'
+
 // finalBoard prop should be an array of objects, one for each player
 function FinalBoard ({ finalBoard }) {
   const [sortedBoard, setSortedBoard] = useState([])
@@ -11,9 +15,9 @@ function FinalBoard ({ finalBoard }) {
   }, [finalBoard])
 
   const background = (i) => {
-    if (i === 1) return '#e0c56e' // gold
-    if (i === 2) return '#d5d5d7' // silver
-    if (i === 3) return '#c5ab84' // bronze
+    // if (i === 1) return '#e0c56e' // gold
+    // if (i === 2) return '#d5d5d7' // silver
+    // if (i === 3) return '#c5ab84' // bronze
     return ''
   }
 
@@ -34,9 +38,14 @@ function FinalBoard ({ finalBoard }) {
             <tbody>
               {
                 sortedBoard?.map((player, index) => (
-                  <tr key={player.player_id} style={{ color: 'black', backgroundColor: background(index + 1) }}>
+                  <tr key={player.player_id} style={{ backgroundColor: background(index + 1) }}>
                     {/* <td>{(index + 1) + '.'}</td> */}
-                    <td><img src='../assets/gold_medal_icon.svg'></img></td>
+                    <td>
+                      { index === 0 && <Gold /> }
+                      { index === 1 && <Silver /> }
+                      { index === 2 && <Bronze /> }
+                      { index >= 3 && (index + 1) + '.'}
+                    </td>
                     <td>{player.name}</td>
                     <td>{player.score}</td>
                     <td>{player.success_ratio}</td>
