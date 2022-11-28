feb6b456c9cf53a3b5ed7fbf154e694008a235eb
Andrey Popov <ap4220@ic.ac.uk>
2022-11-28 14:25:49 +0000

    debugged dynamodb put item

--- aws/lambda_function.py
+++ aws/lambda_function.py
@@ -5,16 +5,22 @@ import requests
 dynamodb = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
-    print(r = requests.get('http://google.com?q=helloworld'))
+    print(requests.get('http://google.com?q=helloworld'))
 
     dynamodb.put_item(
         TableName='Games', 
         Item={
+            'game_id': {
+                'S': 'alkdfjbasldfjn'
+            },
             'fruitName':{
                 'S':'Banana'
             },
-            'key2':{
-                'N':'value2'
+            'vegetableName':{
+                'S':'Potato'
+            },
+            'exampleNumber':{
+                'N':'123'
             }
         }
     )
@@ -23,3 +29,7 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
+
+
+if __name__ == "__main__":
+    lambda_handler(None, None)
