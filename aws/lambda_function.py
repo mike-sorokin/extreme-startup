45512c95b50470c12324f6c1eaee3d5db967c023
Andrey Popov <ap4220@ic.ac.uk>
2022-11-28 14:11:25 +0000

    trying to modify dynamodb

--- aws/lambda_function.py
+++ aws/lambda_function.py
@@ -1,9 +1,23 @@
 import json
-
+import boto3
 import requests
+
+db = boto3.client('dynamodb').Table("Games")
 
 def lambda_handler(event, context):
     print(r = requests.get('http://google.com?q=helloworld'))
+
+    db.put_item(
+        Item={
+            'game_id':{
+                'S':'Banana'
+            },
+            'players':{
+                'N':'value2'
+            }
+        }
+    )
+
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
