78b83ac15fdfa7fe5017ed491467a2b3c966a922
dksethia <dksethia1@gmail.com>
2022-11-30 16:00:13 +0000

    Testing sqs with lambda
    Co-authored-by: Popov, Andrey <andrey.popov20@imperial.ac.uk>

--- aws/lambda_function.py
+++ aws/lambda_function.py
@@ -1,34 +1,75 @@
 import json
 import boto3
 import requests
+from flaskr.shared.dynamo_db import *
 
 dynamodb = boto3.client('dynamodb')
+sqs_resource = boto3.resource('sqs')
+queue = sqs_resource.get_queue_by_name(QueueName='GameTasks')
+
 
 def lambda_handler(event, context):
-    print(requests.get('http://google.com?q=helloworld'))
 
-    dynamodb.put_item(
-        TableName='Games', 
-        Item={
-            'game_id': {
-                'S': 'alkdfjbasldfjn'
+    print(event)
+    counter = int(event['Counter'])
+
+    print("The counter is", counter)
+
+    counter -= 1
+
+    if counter <= 0:
+        return
+
+    queue.send_message(
+        DelaySeconds=1,
+        MessageAttributes={
+            'GameID': {
+                'StringValue': 'test_id',
+                'DataType': 'String'
             },
-            'fruitName':{
-                'S':'Banana'
+            'Counter': {
+                'StringValue': str(counter),
+                'DataType': 'Number'
             },
-            'vegetableName':{
-                'S':'Potato'
-            },
-            'exampleNumber':{
-                'N':'123'
+            'MessageType': {
+                'StringValue': 'AdministerQuestion',
+                'DataType': 'String'
             }
         }
     )
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    return
+
+# if db_is_game_paused(event["game_id"]):
+#     # put event back on the queue and return?
+#     pass
+
+# # 1. Send request to player, and check response
+# question = event["question"]
+# try:
+#     response = requests.get(event["player_api"], params={"q": question["text"]})
+
+#     if response.status_code == 200:
+#         answer = response.text.strip().lower()
+#     else:
+#         answer = "ERROR_RESPONSE"
+# except Exception:
+#     answer = "NO_SERVER_RESPONSE"
+
+# # Allows cheating for demo/test purposes only
+# if event["ALLOW_CHEATING"] and answer == "cheat":
+#     result = "CORRECT"
+# # Check response is correct
+# # elif answer == event["correct_answer"]:
+# #     result = "CORRECT"
+# # else:
+# #     result = "WRONG"
+# result = answer == question["correct_answer"]
+
+# # 2. Update database based on result
+# if result:
+#     pass
+#     # db_correct_answer(game_id, player_id, question)?
 
 
 if __name__ == "__main__":
